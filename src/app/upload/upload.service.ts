5189503306a0cfd789911df954966d4ca179b0f7
ashuanhadgit <ashutosh165rai@gmail.com>
2018-12-19 16:27:24 +0530

    Final app

--- src/app/upload/upload.service.ts
+++ src/app/upload/upload.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/c
 import { Subject } from 'rxjs';
 import { Observable } from 'rxjs';
 
+// Update this URL with your REST API URL which would handle this file uploading
 const url = 'http://localhost:3000/upload';
 
 @Injectable()
@@ -14,35 +15,56 @@ export class UploadService {
     const status = {};
 
     files.forEach(file => {
-      // create a new multipart-form for every file
+      // create a new multipart-form for every file which we want to upload
       const formData: FormData = new FormData();
       formData.append('file', file, file.name);
-
-      // create a http-post request and pass the form
-      // tell it to report the upload progress
+      // Create a http-post request and pass the form
+      // Also tell it to report the upload progress
       const req = new HttpRequest('POST', url, formData, {
         reportProgress: true
       });
 
-      // create a new progress-subject for every file
+      // create a new upload-progress-subject for every file
       const progress = new Subject<number>();
 
-      // send the http-request and subscribe for progress-updates
-
-      let startTime = new Date().getTime();
+      // send the http-request and subscribe for upload-progress-updates
       this.http.request(req).subscribe(event => {
-        if (event.type === HttpEventType.UploadProgress) {
-          // calculate the progress percentage
 
-          const percentDone = Math.round((100 * event.loaded) / event.total);
-          // pass the percentage into the progress-stream
+        // ========================================================================================
+
+        // +++++++++++++++++++++++This is the code which would work with actual server++++++++++++++
+        //   if (event.type === HttpEventType.UploadProgress) {
+        //     // calculate the progress percentage
+
+        //     const percentDone = Math.round((100 * event.loaded) / event.total);
+        //     // pass the percentage into the progress-stream
+        //     progress.next(percentDone);
+        //   } else if (event instanceof HttpResponse) {
+        //     // Close the progress-stream if we get an answer form the API
+        //     // The upload is complete
+        //     progress.complete();
+        //   }
+        // ========================================================================================
+
+        // ========================================================================================
+        // Dummy code to demonstrate the actual look and feel of the uploader 
+        var eventLoaded = 0;
+        var interval = setInterval( increment, 1000);
+        function increment(){
+          eventLoaded = eventLoaded % 360 + 1;
+          const percentDone = Math.round((100 * eventLoaded) / 5);
+          console.log(percentDone);
           progress.next(percentDone);
-        } else if (event instanceof HttpResponse) {
-          // Close the progress-stream if we get an answer form the API
-          // The upload is complete
-          progress.complete();
+
+          if(percentDone == 100) {
+            progress.complete();
+            clearInterval(interval)
+          }
         }
+        //++++++++++++++++++++++++++++++++++++++++Dummy code ends+++++++++++++++++++++++++++++++++ 
+        // ========================================================================================
       });
+
 
       // Save every progress-observable in a map of all observables
       status[file.name] = {
